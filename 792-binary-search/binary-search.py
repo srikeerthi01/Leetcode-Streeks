7dc150507d92a54b7da3c9b0356d46d5ed8a1f75
srikeerthi01 <srikeerthigorripati@gmail.com>
2026-08-27 10:51:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (37.61%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        low,high = 0,len(nums)-1
+        
+        while(low<=high):
+            mid = (low+high)//2
+            if(nums[mid] == target):
+                return mid
+            elif(nums[mid] > target):
+                high = mid-1
+            elif(nums[mid] < target):
+                low = mid+1
+        return -1               
+            
+        
+        
