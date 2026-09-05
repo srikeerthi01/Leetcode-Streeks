b6a952537b5d6bdc17c9e5e5f9d5444eba559a98
srikeerthi01 <srikeerthigorripati@gmail.com>
2026-09-05 20:04:52 +0530

    Time: 328 ms (77.42%) | Memory: 21.7 MB (67.74%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+        right_min = [0] * n
+        right_min[-1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            right_min[i] = min(nums[i], right_min[i + 1])
+        left_max = nums[0]
+        for i in range(n):
+            left_max = max(left_max, nums[i])
+
+            if left_max - right_min[i] <= k:
+                return i
+        return -1      
+                
+        
