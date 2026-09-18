f13c3e487b18a4a964aad618f9a1329d4d4eedea
srikeerthi01 <srikeerthigorripati@gmail.com>
2026-09-18 10:42:43 +0530

    Time: 39 ms (56.21%) | Memory: 13.5 MB (31.90%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -1,11 +1,13 @@
-class Solution:
-    def removeDuplicates(self, s: str) -> str:
+class Solution(object):
+    def removeDuplicates(self, s):
         stack = []
         for ch in s:
-            if stack and stack[-1] == ch:  #stack[-1] used to check with the ch is eaqaul to the last inserted element , we used stack because the stack is previously empty
+            if not stack:
+                stack.append(ch)
+            elif stack[-1] == ch:
                 stack.pop()
             else:
                 stack.append(ch)
         return "".join(stack)
-         
+
         
