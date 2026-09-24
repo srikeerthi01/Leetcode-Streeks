52729ead2c17900b2bb343fefa063f89edb81b8b
srikeerthi01 <srikeerthigorripati@gmail.com>
2026-09-24 11:53:57 +0530

    Time: 582 ms (75.01%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select c.name as Customers from Customers c where (select count(*) from Orders o where o.customerID = c.id)= 0;
+select c.name as Customers from
+Customers c left join Orders o
+on c.id = o.customerId
+where o.id is null;
